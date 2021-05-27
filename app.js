4e49333e534996d11807355dc95c1f70f3c6c49d
Maciej Łącki <maciej.lacky@gmail.com>
2021-05-27 20:58:58 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+TweenMax.defaultEase = Linear.easeOut;
+
+new fullpage("#fullpage", {
+  //options here
+  autoScrolling: true,
+  navigation: true,
+  onLeave: (origin, destination, direction) => {
+    const section = destination.item;
+    const title = section.querySelector("h1");
+    const tl = new TimelineMax({ delay: 0.5 });
+    tl.fromTo(title, 0.5, { y: "50", opacity: 0 }, { y: "0", opacity: 1 });
+    if (destination.index === 1) {
+      const img = document.querySelectorAll(".chair");
+      const description = document.querySelector(".description");
+      tl.fromTo(img, 0.7, { x: "100%" }, { x: "-10%" })
+        .fromTo(
+          description,
+          0.5,
+          { opacity: 0, y: "50" },
+          { y: "0", opacity: 1 }
+        )
+       
+    }
+    if (destination.index === 2) {
+      const img = document.querySelectorAll(".img-two");
+      const description = document.querySelector(".description");
+      tl.fromTo(img, 0.7, { y: "130%" }, { y: "0%" })
+        .fromTo(
+          description,
+          0.5,
+          { opacity: 0, y: "50" },
+          { y: "0", opacity: 1 }
+        )
+       
+    }
+  }
+});
